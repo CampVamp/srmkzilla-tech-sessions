25bd6019ca45923b6ca497d4ea75d73ec8e73b7c
Ajay Ram <ajayramsaravanan4@gmail.com>
2022-10-22 14:48:40 +0530

    Add a file for array methods

---
+++ array.js
@@ -0,0 +1,11 @@
+let arr = [34,52,77];
+
+// let add10 = (arr.forEach((item)=>{
+//     item+=10;
+// }))
+
+arr.forEach((item)=>{
+    item+10;
+})
+
+console.log(arr);
---
+++ lowercase.js
@@ -0,0 +1,7 @@
+lower = (text)=>{
+    return text.toLowerCase();
+}
+
+// module.exports.default(lower);
+// module.exports.lower;
+module.exports = lower;
